b47c8440193f76243e197af5e1a703352b84aa97
68656865 <mshuhaib248@gmail.com>
2025-09-15 06:02:09 -0700

    message

--- views/sponsors-program-view/program-details.php
+++ views/sponsors-program-view/program-details.php
@@ -801,7 +801,7 @@ FROM program_pamphlets WHERE program_id = ?");
             <?php endif; ?>
         <?php else: ?>
             <div class="alert alert-info my-5 text-center" style="font-size:1.2rem;">
-                There is no program details.
+                There is no sponsorship details.
             </div>
         <?php endif; ?>
     </div>
